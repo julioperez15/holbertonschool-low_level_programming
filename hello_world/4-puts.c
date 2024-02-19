3bcdf8eaf58160acf6ee566554b541c11470313f
Julio <julio.1515.pr@gmail.com>
2024-02-19 19:58:00 -0400

    task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 
 int main(void)
+
+/**
+ * File: 4-puts
+ * Auth: Julio Perez
+ */
+
+
 {
 
 	puts("\"Programming is like building a multilingual puzzle");
 
 
-	return(0)
+	return(0);
 
 }
