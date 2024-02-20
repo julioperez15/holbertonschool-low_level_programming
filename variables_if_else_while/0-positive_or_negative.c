a3069ecbc3be6b0f2d434da37436cbb72b0a19a2
Julio <julio.1515.pr@gmail.com>
2024-02-20 19:21:17 -0400

    0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -20,11 +20,11 @@ int main(void)
 	}
 	else if (n > 0)
 	{
-		printf("%d geater than zero\n", n);
+		printf("%d is positive\n", n);
 	}
 	else
 	{
-		printf("%d is less than zero\n", n);
+		printf("%d is negative\n", n);
 	}
 	return (0);
 }
