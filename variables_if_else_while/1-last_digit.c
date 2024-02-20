0543b8cfffd6d5d214e57ae4582217ea7a414d68
Julio <julio.1515.pr@gmail.com>
2024-02-20 16:43:44 -0400

    0-positive_or_negative.c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,14 +1,30 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
 
-/* betty style doc for function main goes there */
+/** main - Entry port
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+	if (n == 0)
+	{
+		printf("%d if the last digit of n is zero the string is 0\n", n);
+	}
+	else if (n > 5)
+	{
+		printf("%d if the last digit of n is grater than 5 the string and is greater than 5\n", n);
+	}
+	else
+	{
+	printf("%d if the last digit of n is less than 6 and not zero the string and is lees than 6 and not zero\n" ,n);
+	}
+
 	return (0);
 }
