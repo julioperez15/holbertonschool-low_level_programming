858af5ff857c985fd9670f854d06ef80834cf3fc
Julio <julio.1515.pr@gmail.com>
2024-02-21 19:02:29 -0400

    task 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+include "main.h"
+
+/**
+ * main - check the function.
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+
+int main(void)
+{
+	char c;
+
+	for (c = a ; c <= z ; c++)
+
+	{
+		_putchat(c);
+	}
+	{
+		_putchar('\n');
+	}
+	return (0);
+}
