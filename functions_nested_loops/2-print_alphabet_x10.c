14957692104aba0baf1e1f39f2f75bc4a50ef083
Julio <julio.1515.pr@gmail.com>
2024-02-22 10:42:48 -0400

    task 2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -5,17 +5,17 @@
  *
  * Return: Always 0
  */
-void print_alphabet_10x(void)
+void print_alphabet_x10(void)
 {
 	char c;
 	int i;
 
 	for(i = 0 ; i < 10 ; i++)
-   {
-	for (c = 'a' ; c <= 'z' ; c++)
 	{
-		_putchar(c);
-	}
+		for (c = 'a' ; c <= 'z' ; c++)
+		{
+			_putchar(c);
+		}
 		_putchar('\n');
-   }
+	}
 }
