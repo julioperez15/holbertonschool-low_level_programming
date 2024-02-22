2dd43cdea14714c097036c811f5f75e6ec94def3
Julio <julio.1515.pr@gmail.com>
2024-02-22 18:05:14 -0400

    task 0

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * void print_numbers(void) : In this function we will prints numbers from 0 to 9
+ * void print_numbers(void) : this function will prints numbers from 0 to 9
  * @x : variable
  * Return : always return 0
  **/
@@ -9,7 +9,8 @@
 void print_numbers(void)
 {
 	int x;
-	_putchar(x = 0; x <= 9 ; x++);
+
+	_putchar(x = 0 ; x <= 9 ; x++);
 
 	{
 		_putchar(x + '0');
