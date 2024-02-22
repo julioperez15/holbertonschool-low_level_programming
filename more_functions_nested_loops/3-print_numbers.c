f39fe2f76298b0d9cdc4ef6fe66b028f66337ed1
Julio <julio.1515.pr@gmail.com>
2024-02-22 14:08:20 -0400

    task 0

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * void print_numbers(void) : In this function we will prints numbers from 0 to 9
+ * Return : always return 0
+ **/
+
+int void print_numbers(void)
+{
+	_putchar( >= 48);
+	_putchar( <= 57);
+	return (0);
+}
