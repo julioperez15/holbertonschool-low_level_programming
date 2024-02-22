c1926b9da76ff5f0df69efd01ad6fffaa45ad04c
Julio <julio.1515.pr@gmail.com>
2024-02-22 17:55:35 -0400

    task 0

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -6,13 +6,13 @@
  * Return : always return 0
  **/
 
-int void print_numbers(void)
+void print_numbers(void)
 {
 	int x;
 	_putchar(x = 0; x <= 9; x++)
 
 	{
-		_putchar(x + 0);
+		_putchar(x + '0');
 	}
-	_putchar('\n')
+	_putchar('\n');
 }
