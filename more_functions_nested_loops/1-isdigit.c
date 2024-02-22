9a02f437d972b63456948b9baccf1698d575cfa3
Julio <julio.1515.pr@gmail.com>
2024-02-22 12:26:03 -0400

    task 0

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - Check if a character is a digit
+ *
+ * Return: 1 for chatacter that will be a digit or 0 for any else
+ */
+
+int _isdigit(int x)
+
+{
+	if (x>= 48 && x<=57)
+	{
+		return (1);
+	}
+	return (0);
+}
