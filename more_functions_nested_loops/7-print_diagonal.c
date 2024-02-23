e7989e24d2e87c9f1f7d35b42a21aa9fa151417a
Julio <julio.1515.pr@gmail.com>
2024-02-22 20:23:28 -0400

    task 0

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -2,15 +2,28 @@
 
 /**
  * print_diagonal - code will print diagonal line.
- * Return : always 0
+ * @n : the number of times of the print
+ * Return : empty
  */
 
 void print_diagonal(int n)
 
 {
-	print_diagonal(0);
-	print_diagonal(2);
-	print_diagonal(10);
-	print_diagonal(-4);
-	return (0);
+	int x, y;
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0 ; x < n ; x++)
+		{
+			for (y = 0 ; y < x ; y++)
+			{
+				_putchar(32);
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
 }
