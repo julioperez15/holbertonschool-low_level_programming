7d4fe2e60d49638a7f48c2220de8aa910c5eaf9d
Julio <julio.1515.pr@gmail.com>
2024-02-22 20:14:59 -0400

    task 0

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_diagonal - code will print diagonal line.
+ * Return : always 0
+ */
+
+void print_diagonal(int n)
+
+{
+	print_diagonal(0);
+	print_diagonal(2);
+	print_diagonal(10);
+	print_diagonal(-4);
+	return (0);
+}
