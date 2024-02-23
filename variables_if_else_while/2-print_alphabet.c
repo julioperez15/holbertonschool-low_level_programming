92195988193965d93cbbf0d1bfebbae16b20e0c7
Julio <julio.1515.pr@gmail.com>
2024-02-23 11:56:23 -0400

    task 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print the alphabet
+ * Return : always success
+ */
+
+int main(void)
+
+{
+	char x;
+
+	for (x = 'a' ; x <= 'z' ; x++)
+	{
+		putchar(x);
+	}
+	return (0);
+}
