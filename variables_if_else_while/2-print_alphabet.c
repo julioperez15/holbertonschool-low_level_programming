b17cd20d4a11fcb83353d367d9c39db4969a9449
Julio <julio.1515.pr@gmail.com>
2024-02-24 17:38:27 -0400

    task 2 fix

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -9,9 +9,11 @@ int main(void)
 {
 	char x;
 
-	for (x = 'a' ; x <= 'z' ; x++)
-	{
-		putchar(x);
-	}
-	return (0);
+for (x = 'a' ; x <= 'z' ; x++)
+{
+putchar(x);
+}
+putchar('\n');
+
+return (0);
 }
