2a03c4f202881d44163f3627c5c913f48eaafb6c
Julio <julio.1515.pr@gmail.com>
2024-02-24 19:47:22 -0400

    task 4

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print all single digit from 0
+ * return : always 0
+ */
+
+int main(void)
+
+{
+	char number;
+
+	int a = 48;
+	int b = 57;
+	
+	for (number = a ; number <= b ; number++)
+	{
+		printf("%d", number);
+	}
+	return (0);
+}
