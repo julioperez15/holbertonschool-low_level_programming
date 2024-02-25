2597db3ba979421dcdc5991dcccf0d6ba0d9efe2
Julio <julio.1515.pr@gmail.com>
2024-02-24 21:22:14 -0400

    task 4

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print all sigle digits from 0.
+ * return : always 0.
+ */
+
+int main(void)
+
+{
+	int number;
+
+		for (number = 30 ; number <= 39 ; number++)
+		{
+			putchar(number);
+			putchar('\n');
+		}
+		return (0);
+}
