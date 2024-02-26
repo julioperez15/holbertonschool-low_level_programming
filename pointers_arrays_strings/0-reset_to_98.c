0d6be5e960562255c7910bf7e65aae99ec688686
Julio <julio.1515.pr@gmail.com>
2024-02-26 12:28:22 -0400

    task 0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main : check the code.
+ * description : function of a pointer to an int as parameter and updates the value it points to 98
+ * return : zero
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
