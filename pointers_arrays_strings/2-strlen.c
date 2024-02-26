720a816d59b05f6c547229c47b7fd3285d0f89b2
Julio <julio.1515.pr@gmail.com>
2024-02-26 19:13:24 -0400

    task 0

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main : check the code.
+ * description : return the length of a string.
+ * return : always 0
+ */
+int _strlen(char *s)
+{
+	printf("%d\n", *s);
+	return (0);
+}
