b41866c1c01528edc403959faa6f29edd604b18c
Julio <julio.1515.pr@gmail.com>
2024-02-28 10:52:52 -0400

    task 0

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcat : check the code
+ * description : concatenats two strings
+ * return : a pointer to the result string dest.
+ */
+char *_strcat(char *dest, char *src)
+{
+int size = 0, x;
+
+while (dest[size])
+	{
+		size++;
+	}
+
+for (x = 0 ; x < size + 1 && src [x] ; x++)
+	{
+		dest[size] = src[x];
+	}
+	return (dest);
+}
+
