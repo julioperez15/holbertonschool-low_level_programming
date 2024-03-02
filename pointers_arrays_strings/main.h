15a8b2be113e688a1bd78ab2c84ee69d78e36480
Julio <julio.1515.pr@gmail.com>
2024-03-02 13:40:48 -0400

    task 2

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -25,6 +25,9 @@ char *cap_string(char *);
 char *leet(char *);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
 #endif
 
 
