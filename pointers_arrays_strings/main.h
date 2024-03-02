db80e73a40e063ce44e198d736493a95d2adc784
Julio <julio.1515.pr@gmail.com>
2024-03-02 10:05:40 -0400

    task 1

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -16,5 +16,6 @@ char *_strcat(char *dest, char *src);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
+char *_strncat(char *dest, char *src, int n);
 #endif
 
