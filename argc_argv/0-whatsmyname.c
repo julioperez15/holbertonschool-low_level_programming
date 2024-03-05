fb210ca9e65367d4956950123acbe62059fcd6f4
Julio <julio.1515.pr@gmail.com>
2024-03-05 11:24:43 -0400

    task 0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -3,13 +3,14 @@
 #include "main.h"
 /**
  * function - a program that print his name
- *
+ * @argc : number of agumets
+ * @argv : argument vector pointer to the string
  * Return : 0
  */
 int main(int argc, char *argv[])
 {
 	(void) argc;
 
-	printf("%s/n", argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
