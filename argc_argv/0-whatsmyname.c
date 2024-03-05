7ea7e2688f7367d948ca5b4167a40041db3030d9
Julio <julio.1515.pr@gmail.com>
2024-03-05 11:15:58 -0400

    task 6

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * function - a program that print his name
+ *
+ * Return : 0
+ */
+int main(int argc, char *argv[])
+{
+	(voi)d argc;
+
+	printf("%s/n", *argv[0]);
+	return (0);
+}
