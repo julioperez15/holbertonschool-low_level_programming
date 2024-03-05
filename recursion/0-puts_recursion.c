b2008100ae508978435c78a63d2b9038326cf968
Julio <julio.1515.pr@gmail.com>
2024-03-04 20:10:14 -0400

    task 0

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,11 +1,12 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * _puts_recursion - function that print a string
  * @*s : straing to be copied.
  *
  * Return : 0
  */
+
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
