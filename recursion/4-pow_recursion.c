8c8c97d9c61761bb375d5853865102bdc7780ebb
Julio <julio.1515.pr@gmail.com>
2024-03-04 21:16:13 -0400

    task 4

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _pow_recursion - to return the value x raised to the power of y
+ * @x: the value to multiply
+ * @y: the times to multiply the value
+ *
+ * Return: integer value
+ */
+int _pow_recursion(int x, int y)
+{
+
+        if (y < 0)
+                return (-1);
+
+        if (y == 0)
+                return (1);
+
+        return (x * _pow_recursion(x, y - 1));
+}
