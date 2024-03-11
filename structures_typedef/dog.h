872c1901a61d5378cb041f1c4e7210609c693155
Julio <julio.1515.pr@gmail.com>
2024-03-11 13:15:53 -0400

    task 0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * struct dog my_dogi - a user defined data type.
+ * @name : the user name
+ * @age : user age
+ * @owner : the owner
+ *
+ * Return : 0
+ */
+struct dog
+{
+  char *name;
+  float age;
+  char *owner;
+};
