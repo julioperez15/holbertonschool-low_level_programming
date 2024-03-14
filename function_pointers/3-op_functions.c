2f1dc16d9741f6fbd5f535cd14b99e3f733f5b51
Julio <julio.1515.pr@gmail.com>
2024-03-13 23:10:38 -0400

    task 0

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,33 @@
+/**
+ * op_add - to add two integer
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+        return (a + b);
+}
+/**
+ * op_sub - to subtract two integer
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: difference
+ */
+int op_sub(int a, int b)
+{
+        return (a - b);
+}
+/**
+ * op_mul - to multiply two integer
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: product
+ */
+int op_mul(int a, int b)
+{
+        return (a * b);
+}
